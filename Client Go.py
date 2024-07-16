fe57fd060aaf6cc6f293cb64b6dc3c428aa8e622
Diego Vargas <fallaandres1007f@gmail.com>
2024-07-16 19:13:11 -0300

    actualizando

---
+++ Client Go.py
@@ -0,0 +1,12 @@
+# aqui estara el inicio de el codigo
+from clases_client import Iniciosesion
+from data_base import DDBB
+
+# PRIMERO CREAMOS LA BASE DE DATOS Y LUEGO SE EJECUTA EL PROGRAMA
+DDBB()
+
+
+if __name__ == "__main__":
+    # inicio sesion
+    iniciar = Iniciosesion()
+    iniciar.inicio()
